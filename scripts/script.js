a197291cf4ca0a274ea5249b2c9da911a1426a7e
Tobies <37544558+Tobies@users.noreply.github.com>
2021-07-26 08:23:06 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -112,7 +112,7 @@ function symbolize(code) {
     var text = unsymbolize(r)
     if (isValid(code)) {
       if (text.length < maxCap) {
-        prompt("מקם את הפינות מחדש", "cap")
+        prompt("מקם את הפינות מחדש", text.length + ", " + maxCap)
       } else if (text.includes(" ")) {
 prompt("space", "space")
 }else {
